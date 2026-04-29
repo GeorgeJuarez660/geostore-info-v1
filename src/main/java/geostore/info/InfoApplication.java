c5d5afb159dff97a107f9503c2fdc31f6538b6c8
GeorgeJuarez <giorgionocerino01@gmail.com>
2026-04-29 12:29:13 +0200

    Set up progetto spring boot
    Inizializzato il progetto spring boot, aggiungendo alcune dipendenze necessarie quali: spring boot jpa, security, validatio, mvc e il plugin lombok

---
+++ src/main/java/geostore/info/InfoApplication.java
@@ -0,0 +1,13 @@
+package geostore.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoApplication.class, args);
+	}
+
+}
